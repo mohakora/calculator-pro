73913e8b69c546bc5d856a2ed9262c20b96cd5d5
mohakora <77640915+mohakora@users.noreply.github.com>
2026-07-01 00:02:17 +0300

    Update config.jshqhtm vf' data

--- assets/config.js
+++ assets/config.js
@@ -3,7 +3,7 @@
 // عدّل القيمتين التاليتين بالقيم الخاصة بمشروعك في Supabase:
 // Project Settings -> API -> Project URL / anon public key
 // ============================================================
-const SUPABASE_URL = "https://YOUR-PROJECT-REF.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR-ANON-PUBLIC-KEY";
+const SUPABASE_URL = "https://uivdggyetztcmxogcaob.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpdmRnZ3lldHp0Y214b2djYW9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI4NDE5MjMsImV4cCI6MjA5ODQxNzkyM30.qH00B5Q6u6HpaREmlxcfMQq5BjFEFSlraFmwc-AeGMc";
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
